8165536e290225f42da24d9bd3e5613d822256c3
Neeluri-sreenivasulu <153499439+Neeluri-sreenivasulu@users.noreply.github.com>
2024-10-11 18:01:06 +0530

    committing slider changes to slider

---
+++ slider.js
@@ -0,0 +1,15 @@
+// Hero-courosel 
+const slider1=document.querySelector("#glide1");
+console.log(slider1)
+if (slider1) {
+    new Glide(slider1, {
+        type: 'carousel',
+        startAt: 0,
+        autoplay: 3000,
+        gap: 0,
+        hoverpause:true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFunc: 'linear'
+    }).mount();
+}
